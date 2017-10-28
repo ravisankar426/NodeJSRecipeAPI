d2007ef36cfc8c56a3a55f730828cd0bcc9e8a62
ravisankar426 <ravisankar426@gmail.com>
2017-10-28 00:43:31 -0400

    Changes made to add CORS and grunt.js

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ var app=express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 const port=3000;
 const hostname='127.0.0.1';
